6bd5ebd82ba0f14e70fc62c41ad3eab1ac52b1a1
sxyugao <sxyugao@qq.com>
2019-09-21 16:07:28 +0800

    update

---
+++ Codeforces/CF9D/CF9D.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+int n, h;
+long long f[40][40];
+int main() {
+  scanf("%d%d", &n, &h);
+  for (int i = 0; i <= n; i++) f[0][i] = 1;
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= n; j++)
+      for (int k = 0; k < i; k++)
+        f[i][j] += f[k][j - 1] * f[i - k - 1][j - 1];
+  printf("%lld", f[n][n] - f[n][h - 1]);
+}
